4c2ccb7ed9cf4b338bc43b1f62b6e3fee6258e6c
Dylan <gustafsondk@gmail.com>
2024-01-17 15:59:39 -0800

    Add files via upload

--- psieve32.c
+++ psieve32.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+//#include <omp.h>
 
 const char* output_name = "primes.dat";
-unsigned int static_primes[54] =
-    {  2,  3,  5,  7, 11, 13, 17, 19, 23,
-      29, 31, 37, 41, 43, 47, 53, 59, 61,
-      67, 71, 73, 79, 83, 89, 97,101,103,
-     107,109,113,127,131,137,139,149,151,
-     157,163,167,173,179,181,191,193,197,
-     199,211,223,227,229,233,239,241,251};
+unsigned int static_primes[] = {3,  5,  7, 11, 13, 17, 19, 23,
+                           29, 31, 37, 41, 43, 47, 53, 59, 61,
+                           67, 71, 73, 79, 83, 89, 97,101,103,
+                          107,109,113,127,131,137,139,149,151,
+                          157,163,167,173,179,181,191,193,197,
+                          199,211,223,227,229,233,239,241,251};
 
 int rule_out(const unsigned int* pfacs, unsigned int n, char* skip, unsigned int max)
 {
@@ -19,7 +18,8 @@ int rule_out(const unsigned int* pfacs, unsigned int n, char* skip, unsigned int
     {
         p = pfacs[i];
 
-        for(j = 2 * p; j < max && j >= 2 * p; j += p)
+        //#pragma omp parallel for
+        for(j = (3 * p - 1) / 2; j < max; j += p)
             skip[j] = 1;
     }
 
@@ -33,7 +33,7 @@ int rule_out(const unsigned int* pfacs, unsigned int n, char* skip, unsigned int
 
 int main(int argc, char* argv[])
 {
-    unsigned int i, j;
+    unsigned int i, j, p;
 
     FILE* outfile = fopen(output_name, "wb");
     if(outfile == NULL)
@@ -42,36 +42,40 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    char* skip = (char*) calloc(UINT_MAX, sizeof(char));
-    
+    char* skip = (char*) calloc(0x80000000, sizeof(char));
     skip[0] = 1;
-    skip[1] = 1;
 
-    unsigned int total_1 = rule_out(static_primes, sizeof(static_primes)/sizeof(int), skip, USHRT_MAX);
-    unsigned int* dynamic_primes = (unsigned int*) malloc(total_1 * sizeof(unsigned int));
+    unsigned int total_16 = rule_out(static_primes, sizeof(static_primes)/sizeof(int), skip, 0x8000);
+    unsigned int* dynamic_primes = (unsigned int*) malloc(total_16 * sizeof(int));
 
     i = 0;
-    for(j = 0; j < USHRT_MAX; j++)
+    for(j = 0; j < 0x8000; j++)
     {
         if(skip[j]) continue;
 
-        dynamic_primes[i] = j;
+        dynamic_primes[i] = 2 * j + 1;
         i++;
     }
 
     puts("Finished sieve 1, starting sieve 2"); fflush(stdout);
-    unsigned int total_2 = rule_out(dynamic_primes, total_1, skip, UINT_MAX);
+    unsigned int total_32 = rule_out(dynamic_primes, total_16, skip, 0x80000000);
     free(dynamic_primes);
-    
+
     puts("Finished sieve 2, writing to file"); fflush(stdout);
-    fwrite(&total_2, sizeof(int), 1, outfile);
-    for(j = 0; j < UINT_MAX; j++)
+
+    total_32++;
+    fwrite(&total_32, sizeof(int), 1, outfile);
+    p = 2;
+    fwrite(&p, sizeof(int), 1, outfile);
+
+    for(j = 0; j < 0x80000000; j++)
     {
-        if(!skip[j]){
-            fwrite(&j, sizeof(int), 1, outfile);
-        }
+        if(skip[j]) continue;
+
+        p = 2 * j + 1;
+        fwrite(&p, sizeof(int), 1, outfile);
     }
-        
+
     free(skip);
     fclose(outfile);
     return 0;
