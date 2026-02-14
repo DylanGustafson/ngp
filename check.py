75f81867149cb5ecc95c5122dbcd33e3ff753fb8
dylan <gustafsondk@gmail.com>
2026-02-13 20:38:59 -0800

    Added feature to read directly from verbose ngp-bin output

--- check.py
+++ check.py
@@ -2,15 +2,44 @@ from sympy.ntheory.residue_ntheory import is_primitive_root
 from sympy.ntheory import isprime, primefactors
 import sys
 
-N = int(sys.argv[-1])
-if not isprime(N):
-    print(f"Input is not prime: {primefactors(N)}")
+def build_line(N):
+    if not isprime(N):
+        print(f"{N} is not prime: {primefactors(N)}")
+        exit()
+
+    for root in range(2, N):
+        if is_primitive_root(root, N):
+            break
+
+    q = (pow(root, N, N * N) - root) // N
+    pfacs = primefactors(N - 1)
+
+    return f"{N} ({root} {q}) " + " ".join([str(i) for i in pfacs])
+
+arg = sys.argv[-1]
+try:
+    N = int(arg)
+    print(build_line(N))
     exit()
 
-for root in range(2, N):
-    if is_primitive_root(root, N):
-        break
+except ValueError:
+    pass
 
-print(f"Minimum primitive root: {root}")
-residue = (pow(root, N - 1, N * N) - 1) // N
-print(f"(r^(p-1) % p^2 - 1) /p: {residue}")
+with open(arg, 'r') as f:
+    lines = f.read().splitlines()
+
+line_count = len(lines)
+for i, line in enumerate(lines):
+    print(f"{i + 1}/{line_count}", end="\r")
+    Nstr = line.split(" ", 1)[0]
+    if not Nstr.isdecimal():
+        continue
+
+    true_line = build_line(int(Nstr))
+    if true_line != line:
+        print("Value Discrepency!")
+        print("Given: " + line)
+        print("True : " + true_line)
+        exit()
+
+print("\nPassed!")
